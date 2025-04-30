4a97462751c635951a4d94ee661f74a584965c33
TediBee <tedi.gunawan@codingbee.org>
2025-04-30 16:20:02 +0700

    changes

--- test.py
+++ test.py
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
+
 nama = input("Masukkan nama Anda: ")
-for i in range(3):  
-    print((nama + " ") * 5)  
-=======
+for i in range(3):
+    print((nama + " ") * 5)
 import os
 
 
 def clear():
-<<<<<<< HEAD
     os.system('cls' if os.name=='nt' else 'clear')
->>>>>>> 3f7ce04a2c13e59b3d2e9e01a28b4c6094a914b4
-=======
-    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 
 print('ini')
->>>>>>> 08459520997ea4f0c85803ed74c391960c80de1d
+
